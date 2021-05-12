85d6979529c7c3dae3c549fad6df1ea8a3e1bf07
Yash Mathur <dev.yashmathur@gmail.com>
2021-05-12 20:56:33 +0530

    coloring based on status and added appropriate colors

--- TamboloApp/src/tambolo/NumStatusRender.java
+++ TamboloApp/src/tambolo/NumStatusRender.java
@@ -1,5 +1,6 @@
 package tambolo;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 
@@ -8,9 +9,11 @@ import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.table.TableCellRenderer;
 
+@SuppressWarnings("serial")
 public class NumStatusRender extends JLabel implements TableCellRenderer {
-	
-	NumStatusRender() {
+	Boolean[][] NUM_STATUS;
+	NumStatusRender(Boolean[][] numStatus_table) {
+		NUM_STATUS = numStatus_table;
 		Font num_font = new Font(Font.MONOSPACED, Font.BOLD, 20);
 		this.setFont(num_font);
 		this.setHorizontalAlignment(SwingConstants.CENTER);
@@ -20,13 +23,20 @@ public class NumStatusRender extends JLabel implements TableCellRenderer {
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
 			int row, int column) {
 		Integer current = (Integer) value;
-		if(current == null) {
-			this.setText("");
-			return this;
-		}
-		
-		else {
-			this.setText(Integer.toString(current));
+		try {
+			if(NUM_STATUS[row][column] == null ||NUM_STATUS[row][column] == false ) {
+				this.setBackground(new Color(204, 255, 204));
+				this.setText("");
+				return this;
+			}
+			
+			else {
+				this.setBackground(new Color(255, 204, 255));
+				this.setText(Integer.toString(current));
+				return this;
+			}
+		}catch (NullPointerException e) {
+			System.out.println("NPE");
 			return this;
 		}
 		
--- TamboloApp/src/tambolo/ticketRender.java
+++ TamboloApp/src/tambolo/ticketRender.java
@@ -6,20 +6,18 @@ import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.TitledBorder;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
 
 @SuppressWarnings("serial")
 class TickTable extends JTable {
-	
-	TickTable(Object[][] contents, Object[] colNames, int dim) {
+	Boolean numStatus_table[][];
+	TickTable(Object[][] contents, Object[] colNames, Boolean ns[][], int dim) {
 		super(contents, colNames);
 		
+		numStatus_table = ns;
 		setSize(450,150);
 		TableColumnModel colModel = this.getColumnModel();
 		for (int column = 0; column < this.getColumnCount(); column++) {
@@ -29,8 +27,9 @@ class TickTable extends JTable {
 		this.setRowHeight(dim);
 		
 		//this.setDefaultEditor(Integer.class, null);
-		this.setDefaultRenderer(Integer.class, new NumStatusRender());
+		this.setDefaultRenderer(Integer.class, new NumStatusRender(numStatus_table));
 		this.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+		
 	}
 	@Override
 	public Class<?> getColumnClass(int column) {
@@ -45,10 +44,12 @@ class TickTable extends JTable {
 
 @SuppressWarnings("serial")
 public class ticketRender extends JPanel{
+	TickTable table;
 	ticketRender (String name) {
 		super();
 		Ticket ob = new Ticket();
-		TickTable table = new TickTable(ob.generator(), Ticket.names, 50);
+		table = new TickTable(ob.generator(), Ticket.names, ob.numStatus, 50);
+		//table.numStatus_table = ob.numStatus; //added to constructor
 		JPanel tableLayer = new JPanel(); //layer to store the table, so as to give it a header
 		
 		tableLayer.add(table);
@@ -57,18 +58,27 @@ public class ticketRender extends JPanel{
 				name, TitledBorder.CENTER, TitledBorder.BELOW_TOP);
 		ticket_name.setTitleFont(name_font);
 		tableLayer.setBorder(ticket_name);
+		tableLayer.setBackground(new Color(204, 221, 238));
 		this.add(tableLayer);
+		
 	}
+	
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.setLayout(new FlowLayout());
 		ticketRender test = new ticketRender("name");
-		ticketRender test2 = new ticketRender("name2");
 		//frame.setLayout(new FlowLayout());
 		frame.add(test);
-		frame.add(test2);
 		frame.pack();
 		frame.setVisible(true);
+		for(int i = 0; i<Ticket.rows; i++) {
+			for(int j = 0; j<Ticket.cols; j++) {
+				System.out.print(test.table.numStatus_table[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
+	
+	
 }
