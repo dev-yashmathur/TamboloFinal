7bae8f18d35ef0994d81671c41303d59c9aed221
Yash Mathur <3182778+dev-yashmathur@users.noreply.github.com>
2021-05-13 23:38:00 +0530

    Added pages:
    1) Name
    2) Prizes
    along with input rule enforcement and keyboard inputs.
    -> Started working on actual gameplay. tickets ready

--- TamboloApp/src/tambolo/UI.java
+++ TamboloApp/src/tambolo/UI.java
@@ -6,6 +6,8 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -13,12 +15,13 @@ import javax.swing.*;
 import javax.swing.border.Border;
 
 public class UI {
-//	JFrame welcome_frame;
 	Font UIFont = new Font(Font.MONOSPACED, Font.BOLD, 48);
 	Font Smaller = new Font(Font.MONOSPACED, Font.ITALIC, 32);
 	Font caution = new Font(Font.SANS_SERIF, Font.BOLD, 24);
 	Color bg = new Color(196, 250, 248);
 	Border Padding = BorderFactory.createEmptyBorder(35, 35, 35, 35);
+	
+	int Tickets_num;
 	
 	void welcome_frame_init() {
 		JFrame welcome_frame = new JFrame();
@@ -54,7 +57,7 @@ public class UI {
 			@Override
 			public void keyReleased(KeyEvent e) {
 				welcome_frame.dispose();
-				first_frame_init();
+				player_details_frame_init();
 			}
 			@Override
 			public void keyTyped(KeyEvent e) { }
@@ -64,8 +67,8 @@ public class UI {
 		});
 	}
 	
-	void first_frame_init() {
-		JFrame first_frame = new JFrame();
+	void player_details_frame_init() {
+		JFrame player_details_frame = new JFrame();
 		JPanel input_panel = new JPanel();
 		input_panel.setBackground(bg);
 		input_panel.setLayout(new GridBagLayout());
@@ -76,10 +79,10 @@ public class UI {
 		caution_label.setFont(caution);
 		caution_label.setForeground(Color.red);
 		
-		JTextField input = new JTextField(10);
-		input.setFont(UIFont);
-		input.setBackground(Color.white);
-		input.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+		JTextField inputNoOfTickets = new JTextField(10);
+		inputNoOfTickets.setFont(UIFont);
+		inputNoOfTickets.setBackground(Color.white);
+		inputNoOfTickets.setBorder(BorderFactory.createLineBorder(Color.black, 2));
 		
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new Insets(10, 10, 10, 10);
@@ -91,10 +94,11 @@ public class UI {
 		//for Text Field
 		gbc.gridx = 1;
 		gbc.gridy = 0;
-		input_panel.add(input, gbc);
+		input_panel.add(inputNoOfTickets, gbc);
 		
+		JButton goToPrizes = new JButton("Select Prizes");
 		//gridy = 1 is used for caution
-		input.addKeyListener(new KeyListener() {
+		inputNoOfTickets.addKeyListener(new KeyListener() {
 			@Override
 			public void keyTyped(KeyEvent e) { }	
 			@Override
@@ -102,30 +106,250 @@ public class UI {
 			@Override
 			public void keyReleased(KeyEvent e) {
 				char current_letter = e.getKeyChar();
+				if(current_letter == 10) {
+					goToPrizes.doClick();
+					return;
+				}
+				
 				if(!Character.isDigit(current_letter) && current_letter != 65535 && current_letter >10 ) {
 					try {
-					String current = input.getText();
+					String current = inputNoOfTickets.getText();
 					current = current.substring(0, current.length()-1);
-					input.setText(current);
+					inputNoOfTickets.setText(current);
 					
 					gbc.gridx = 1;
 					gbc.gridy = 1;
 					input_panel.add(caution_label, gbc);
-					first_frame.revalidate();
-					first_frame.pack();
+					player_details_frame.revalidate();
+					player_details_frame.pack();
 					} catch(Exception excep) {}
 				}
 			}
 			
 		});
+	
 		
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		gbc.gridwidth = 2;
+		input_panel.add(goToPrizes, gbc);		
+		goToPrizes.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Tickets_num = Integer.parseInt(inputNoOfTickets.getText());
+				} catch(Exception excep) {
+					gbc.gridx = 1;
+					gbc.gridy = 1;
+					gbc.gridwidth = 1;
+					input_panel.add(caution_label, gbc);
+					player_details_frame.revalidate();
+					player_details_frame.pack();
+					return;
+				}
+				player_details_frame.dispose();
+				prize_frame_init();
+			}
+		});
 		
-		first_frame.add(input_panel);
-		first_frame.pack();
-		first_frame.setVisible(true);
-		first_frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		player_details_frame.add(input_panel);
+		player_details_frame.pack();
+		player_details_frame.setVisible(true);
+		player_details_frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 	}
+	
+	void prize_frame_init() {
+		JFrame prize_frame = new JFrame();
+		JPanel prize_panel = new JPanel();
+		prize_panel.setBorder(Padding);
+		prize_panel.setBackground(bg);
+		prize_panel.setLayout(new GridBagLayout());
+		JLabel caution_label = new JLabel("Please enter a number");
+		caution_label.setFont(caution);
+		caution_label.setForeground(Color.red);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(10, 10, 10, 10);
+		JLabel introToPrizes = new JLabel("<html><u>Select The Prizes To Play With</u></html>");
+		introToPrizes.setFont(UIFont);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 2;
+		prize_panel.add(introToPrizes, gbc);
+		
+		JCheckBox J5 = new JCheckBox("First 5"); //gridy = 1
+		J5.setFont(Smaller);
+		J5.setBackground(bg);
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridwidth = 2;
+		prize_panel.add(J5, gbc);
+		
+		J5.addKeyListener(new KeyListener() {
 
+			@Override
+			public void keyTyped(KeyEvent e) { }
+			@Override
+			public void keyPressed(KeyEvent e) {}
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if(e.getKeyChar() == 10) {
+					J5.doClick();
+				}
+			}
+			
+		});
+		
+		JCheckBox LINES = new JCheckBox("Lines"); //gridy = 2
+		LINES.setFont(Smaller);
+		LINES.setBackground(bg);
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.gridwidth = 2;
+		prize_panel.add(LINES, gbc);
+		
+		LINES.addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) { }
+			@Override
+			public void keyPressed(KeyEvent e) { }	
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if(e.getKeyChar() == 10) {
+					LINES.doClick();
+				}
+			}
+		});
+		
+		JPanel numOfLines_panel = new JPanel();
+		numOfLines_panel.setBackground(bg);
+		JLabel numOfLines_Label = new JLabel("Enter No. of Lines:");
+		numOfLines_Label.setFont(Smaller);
+		JTextField numOfLines = new JTextField(10); //gridy = 2
+		numOfLines.setFont(Smaller);
+		numOfLines_panel.add(numOfLines_Label);
+		numOfLines_panel.add(numOfLines);
+		numOfLines_panel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+
+		LINES.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(LINES.isSelected()) {
+					gbc.gridx = 1;
+					gbc.gridy = 2;
+					gbc.weightx = 0.3;
+					gbc.gridwidth = 1;
+					prize_panel.add(numOfLines_panel, gbc);
+					prize_panel.remove(LINES);
+					gbc.gridx = 0;
+					gbc.gridy = 2;
+					gbc.gridwidth = 1;
+					gbc.anchor = GridBagConstraints.EAST;
+					prize_panel.add(LINES, gbc);
+					gbc.anchor = GridBagConstraints.CENTER;
+					prize_frame.revalidate();
+					prize_frame.pack();
+				}
+				else {
+					prize_panel.remove(numOfLines_panel);
+					prize_panel.remove(LINES);
+					prize_panel.add(LINES, gbc);
+					prize_frame.revalidate();
+					prize_frame.pack();
+				}
+			}
+		});
+		JButton goToPlay = new JButton("PLAY!");
+		numOfLines.addKeyListener(new KeyListener() { //gridy = 3 = caution
+			@Override
+			public void keyTyped(KeyEvent e) { }	
+			@Override
+			public void keyPressed(KeyEvent e) { }
+			@Override
+			public void keyReleased(KeyEvent e) {
+				char current_letter = e.getKeyChar();
+				if(current_letter == 10) {
+					goToPlay.doClick();
+				}
+				if(!Character.isDigit(current_letter) && current_letter != 65535 && current_letter >10 ) {
+					try {
+					String current = numOfLines.getText();
+					current = current.substring(0, current.length()-1);
+					numOfLines.setText(current);
+					
+					gbc.gridx = 1;
+					gbc.gridy = 3;
+					prize_panel.add(caution_label, gbc);
+					prize_frame.revalidate();
+					prize_frame.pack();
+					} catch(Exception excep) {} //out of bounds
+				}
+			}
+			
+		});
+		
+		gbc.gridx = 0;
+		gbc.gridwidth = 2;
+		gbc.gridy = 4;
+		prize_panel.add(goToPlay, gbc);
+		
+		goToPlay.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int n = -1;
+				if(LINES.isSelected()) {
+					try {
+						n = Integer.parseInt(numOfLines.getText());
+					}catch(Exception excep) {
+						gbc.gridx = 1;
+						gbc.gridy = 3;
+						gbc.gridwidth = 1;
+						prize_panel.add(caution_label, gbc);
+						prize_frame.revalidate();
+						prize_frame.pack();
+						return;
+					}
+					Prizes.LINE_PRIZES = new Boolean[n];
+					for(int i = 0; i<n; i++) {
+						Prizes.LINE_PRIZES[i] = true;
+					}
+				}//do above if LINES is selected
+				prize_frame.dispose();
+				play_frame_init();
+			}
+		});
+		
+		
+		prize_frame.add(prize_panel);
+		prize_frame.pack();
+		prize_frame.setVisible(true);
+		prize_frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+	}
+	
+	void play_frame_init() {
+		JFrame play_frame = new JFrame();
+		JFrame ticket_frame = new JFrame();
+		JPanel play_panel = new JPanel();
+		JPanel ticket_panel = new JPanel();
+		ticket_panel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridy = 0;
+		
+		ticketRender[] ticket_list = new ticketRender[Tickets_num];
+		for(int i = 0; i<Tickets_num; i++) {
+			gbc.gridy++;
+			ticket_list[i] = new ticketRender("demo");
+			ticket_panel.add(ticket_list[i], gbc);
+		}
+		JScrollPane jsp = new JScrollPane(ticket_panel);
+		ticket_frame.add(jsp);
+		ticket_frame.pack();
+		ticket_frame.setVisible(true);
+		ticket_frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		UI ob = new UI();
 		ob.welcome_frame_init();
