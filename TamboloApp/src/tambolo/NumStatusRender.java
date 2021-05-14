0b812d1287eba411d4eab3d3975bd2765ce75d42
Yash Mathur <3182778+dev-yashmathur@users.noreply.github.com>
2021-05-14 23:25:48 +0530

    changed the colors, and fine tuned the way of checking numStatus

--- TamboloApp/src/tambolo/NumStatusRender.java
+++ TamboloApp/src/tambolo/NumStatusRender.java
@@ -11,9 +11,12 @@ import javax.swing.table.TableCellRenderer;
 
 @SuppressWarnings("serial")
 public class NumStatusRender extends JLabel implements TableCellRenderer {
+	int cnt = 0;
+	TickTable tab;
 	Boolean[][] NUM_STATUS;
-	NumStatusRender(Boolean[][] numStatus_table) {
-		NUM_STATUS = numStatus_table;
+	
+	NumStatusRender(TickTable tb) {
+		tab = tb;
 		Font num_font = new Font(Font.MONOSPACED, Font.BOLD, 20);
 		this.setFont(num_font);
 		this.setHorizontalAlignment(SwingConstants.CENTER);
@@ -22,16 +25,30 @@ public class NumStatusRender extends JLabel implements TableCellRenderer {
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
 			int row, int column) {
+		NUM_STATUS = tab.numStatus_table;
+//		for(int i = 0; i<3; i++) {
+//			for(int j = 0; j<9; j++) {
+//				System.out.print(NUM_STATUS[i][j] + "  ");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println(cnt++ + " ");
+//		System.out.println();
 		Integer current = (Integer) value;
 		try {
-			if(NUM_STATUS[row][column] == null ||NUM_STATUS[row][column] == false ) {
+			if(NUM_STATUS[row][column] == null  ) {
 				this.setBackground(new Color(204, 255, 204));
 				this.setText("");
 				return this;
 			}
 			
-			else {
+			else if(NUM_STATUS[row][column] == false){
 				this.setBackground(new Color(255, 204, 255));
+				this.setText(Integer.toString(current));
+				return this;
+			}
+			else {
+				this.setBackground(new Color(204, 255, 204));
 				this.setText(Integer.toString(current));
 				return this;
 			}
