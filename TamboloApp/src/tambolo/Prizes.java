cbebeb96938890bc9377ffbb7c76c2c487cfaf34
Yash Mathur <3182778+dev-yashmathur@users.noreply.github.com>
2021-05-17 00:56:40 +0530

    Added full house count, first to n integer

--- TamboloApp/src/tambolo/Prizes.java
+++ TamboloApp/src/tambolo/Prizes.java
@@ -1,16 +1,14 @@
 package tambolo;
-
-import java.util.ArrayList;
-import java.util.List;
-
 //The variables required for the entire game play
 public class Prizes {
 	
 	//set them in the actionListeners of swing interfaces.
 	//NOTE: null = not present, true = left, false = finished
 	static Boolean LINE_PRIZES[];
-	static Boolean FULL_HOUSE[];
+	static Boolean FULL_HOUSE;
+	static int FULL_HOUSE_NO;
 	static Boolean FIRST_N;
+	static int First_To;
 	static Boolean DAY;
 	static Boolean NIGHT;
 	static Boolean RAILWAYS;
